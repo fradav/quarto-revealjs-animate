dc70d980edab3f7cde6f700f0fe403ce9756010c
François-David Collin <Francois-David.Collin@umontpellier.fr>
2023-06-22 15:49:45 +0200

    pages

--- _extensions/animate/animate.lua
+++
@@ -1,5 +0,0 @@
-function CodeBlock(el)
-    if el.classes[1] == "json" and el.classes[2] == "animate" then
-        return pandoc.RawBlock("html", "<div data-animate data-src=\"".. el.attr.attributes["src"] .. "\" ><!-- " .. el.text .. " --></div>")
-    end
-end
